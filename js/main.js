8b3e9744d1197cefa36477ed670a97300e807ef5
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-07-03 19:55:36 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+
 const prev = document.querySelector('.leftArrow')
 const next = document.querySelector('.rightArrow')
 const mainImg = document.querySelectorAll('img')
@@ -20,3 +21,24 @@ mainImg.forEach(items => {
     })
 })
 
+// let slideIndex = 1
+// showSlides(slideIndex)
+
+// next.addEventListener('click', (n) =>{
+//     showSlides(slideIndex += n)
+// })
+
+
+// function current(n){
+//     showSlides(slideIndex = n)
+// }
+// function showSlides(n){
+//     let i
+//     let myImage = document.getElementsByClassName('image')
+//     if(n > myImage.length) {slideIndex = 1}
+//     if(n < 1){slideIndex = myImage.length}
+//     for( i = 0; i<myImage.length; i++){
+//         myImage[i].style.display = 'none'
+//     }
+//     myImage[slideIndex-1].style.display = 'block'
+// }
